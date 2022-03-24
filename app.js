ef11f1c22f4c26509e285f8b98238f6d626ee080
Khayman King <khaymanak@gmail.com>
2022-03-24 12:36:37 -0700

    app.js sign up form completed

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 // import functions and grab DOM elements
+import {} from './fetch-utils.js';
 
+
+const signUpForm = document.getElementById('.sign-up');
+const loginForm = document.getElementById('.login');
 // let state
 
 // set event listeners 
+
+signUpForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const signUpData = new FormData(signUpForm);
+    const email = signUpData.get('sign-up-email');
+    const password = signUpData.get('sign-up-password');
+    await signUp(email, password);
+
+    window.location.href = './polls';
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
