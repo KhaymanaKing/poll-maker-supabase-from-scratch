fc3ab4521fd45b6d68326205984e7e05f8321264
Khayman King <khaymanak@gmail.com>
2022-03-24 11:12:18 -0700

    fetch js is made

---
+++ fetch.js
@@ -0,0 +1,21 @@
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxnenNmc3Fhb2h0a3Z5d2x1a3NjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDgxMzc4NzMsImV4cCI6MTk2MzcxMzg3M30.om7bZuYtorq0m3djqz_kSD3Nk_4yrHIUjDusyXl-Z9s';
+
+const SUPABASE_URL = "https://lgzsfsqaohtkvywluksc.supabase.co"
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function createPoll(pastPoll){
+    const response = await client
+        .from('poll')
+        .insert('pastpoll');
+
+    return response.body;
+}
+
+export async function getPoll(){
+    const response = await client
+        .from('poll')
+        .insert('*');
+
+    return response.body;
+}
