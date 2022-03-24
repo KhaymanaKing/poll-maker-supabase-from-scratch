7898f92f274c912ce6770c6c9cc7ae7d722b1774
Khayman King <khaymanak@gmail.com>
2022-03-24 10:37:12 -0700

    buttons now working

--- polls/polls.js
+++ polls/polls.js
@@ -1,12 +1,13 @@
 // import
 
 const form = document.getElementById('form');
-const plusOneButton = document.querySelector('.option-one-plus');
-const minusOneButton = document.querySelector('.option-one-minus');
-const plusTwoButton = document.querySelector('.option-two-plus');
-const minusTwoButton = document.querySelector('.option-two-minus');
-const endPollButton = document.querySelector('.end-poll-button');       
-
+const plusOneButton = document.querySelector('#option-one-plus');
+const minusOneButton = document.querySelector('#option-one-minus');
+const plusTwoButton = document.querySelector('#option-two-plus');
+const minusTwoButton = document.querySelector('#option-two-minus');
+const endPollButton = document.querySelector('#end-poll-button');       
+const optionOneVotesEl = document.querySelector('#option-one-votes');
+const optionTwoVotesEl = document.querySelector('#option-two-votes');
 
 let question = '';
 let optOneInput = '';
@@ -17,16 +18,21 @@ let optTwoVotes = 0;
 
 plusOneButton.addEventListener('click', () => {
     optOneVotes++;
+
+    optionOneVotesEl.textContent = optOneVotes;
 });
 
 minusOneButton.addEventListener('click', () => {
     optOneVotes--;
+    optionOneVotesEl.textContent = optOneVotes;
 });
 
 plusTwoButton.addEventListener('click', () => {
     optTwoVotes++;
+    optionTwoVotesEl.textContent = optTwoVotes;
 });
 
 minusTwoButton.addEventListener('click', () => {
     optTwoVotes--;
+    optionTwoVotesEl.textContent = optTwoVotes;
 });
